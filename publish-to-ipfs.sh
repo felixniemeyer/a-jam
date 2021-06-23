b110d4c51cb2944ce9047b0282cdb25c6b7e9a90
Felix Paul Niemeyer <niemeyer.felix@gmail.com>
2021-06-23 10:37:42 +0200

    adding yarn lock and publish script

--- publish-to-ipfs.sh
+++ publish-to-ipfs.sh
@@ -1,2 +1,8 @@
 yarn build
 ipfs add -r --cid-version 1 dist/
+
+echo "you might also want to:"
+echo " - pin the cid on pinata" 
+echo " - update ipns" 
+echo " - manually update unstoppable domains" 
+
