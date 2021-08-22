24cb20057e0b1468208cb2c01bda75b56706706f
Felix Niemeyer <niemeyer.felix@gmail.com>
2021-08-23 00:16:36 +0200

    lint fix https server

--- https-server.js
+++ https-server.js
@@ -1,15 +1,15 @@
-const https = require('https');
-const fs = require('fs');
-const express = require('express');
+const https = require('https')
+const fs = require('fs')
+const express = require('express')
 
 const app = express()
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 const options = {
   key: fs.readFileSync('/home/fairlix/192.168.1.68-key.pem'),
-  cert: fs.readFileSync('/home/fairlix/192.168.1.68.pem'),
-};
+  cert: fs.readFileSync('/home/fairlix/192.168.1.68.pem')
+}
 
 https
   .createServer(options, app)
-  .listen(8440);
+  .listen(8440)
