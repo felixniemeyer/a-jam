50584c821127b6e1b277e58d61af975e69b05424
Felix Niemeyer <niemeyer.felix@gmail.com>
2021-08-22 22:49:11 +0200

    adding a quick https server that statically serves dist

---
+++ https-server.js
@@ -0,0 +1,15 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+
+const app = express()
+app.use(express.static('dist'));
+
+const options = {
+  key: fs.readFileSync('/home/fairlix/192.168.1.68-key.pem'),
+  cert: fs.readFileSync('/home/fairlix/192.168.1.68.pem'),
+};
+
+https
+  .createServer(options, app)
+  .listen(8440);
